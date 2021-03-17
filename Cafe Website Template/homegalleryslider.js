8bcfefbfbe5f6ce0f2b906f681e2a3d416f487b7
VishruthS <71061052+VishruthS@users.noreply.github.com>
2021-03-17 21:26:19 +0530

    Add files via upload

---
+++ Cafe Website Template/homegalleryslider.js
@@ -0,0 +1,56 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slide2Index = 1;
+showSlides2(slide2Index);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slide2Index += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slide2Index = n);
+}
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {slide2Index = 1}
+  if (n < 1) {slide2Index = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+      dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slide2Index-1].style.display = "block";
+  dots2[slide2Index-1].className += " active";
+}
